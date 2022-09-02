371d7fdd2d30fb49f212a9c30e3cc3e52e8c2f37
Divider <it-cio@mail.ru>
2022-09-02 16:55:12 +0300

    Sum all data in xlsx

--- main.py
+++ main.py
@@ -14,12 +14,26 @@ for file in os.listdir(src):
         date = datetime.datetime.fromtimestamp(time)
         file = file.split('-')[1]
         file = file.split('.')[0]
-        file_list.append([file.lower(), date.strftime('%Y-%m-%d'), 'проверка...'])
+        if file.count('+') == 1:
+            status = 'заявка подписана и передана в мто'
+        elif file.count('+') == 2:
+            status = 'заявка отработана, есть договор и счет'
+        elif file.count('+') == 3:
+            status = 'счет подписан и передан в оплату'
+        elif file.count('+') == 4:
+            status = 'товар поставлен'
+        elif file.count('+') == 5:
+            status = 'договор закрыт'
+        elif '=' in file:
+            status = 'заявка готовится к торгам'
+        else:
+            status = ''
+        file_list.append([file.lower(), date.strftime('%Y-%m-%d'), status])
     else:
         print(f'rejected: {file}')
 
-df = pd.DataFrame(file_list, columns=['Наименование', 'Дата', 'Статус'])\
-    .sort_values(by='Дата', ascending=False)
+df = pd.DataFrame(file_list, columns=['Заявка', 'Дата', 'Статус'])\
+    .sort_values(by='Статус', ascending=False)
 
 with pd.ExcelWriter(f'{dst}/registry.xlsx', engine='xlsxwriter') as wb:
     df.to_excel(wb, sheet_name='Registry', index=False, startrow=1, startcol=1)
@@ -27,7 +41,7 @@ with pd.ExcelWriter(f'{dst}/registry.xlsx', engine='xlsxwriter') as wb:
 
     sheet.set_column('B:B', 50)
     sheet.set_column('C:C', 10)
-    sheet.set_column('D:D', 11)
+    sheet.set_column('D:D', 33)
 
 print('Registry created')
 
